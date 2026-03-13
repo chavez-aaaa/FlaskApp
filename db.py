146f506d32bc02ba8a84f124933137cebe06a53c
LAB_B_REAL <labb@continental.edu.pe>
2026-03-13 15:20:03 -0500

    conexion base de datos railway

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
+import os
 import mysql.connector
 
 def get_connection():
-# Creamos una conexión a MySQL
+    # Conectamos usando las variables de entorno
     return mysql.connector.connect(
-        host="localhost", # Servidor
-        user="root", # Usuario
-        password="", # Contraseña
-        database="flask_db" # Base de datos
+        host=os.environ.get("DB_HOST", "localhost"),       # Host de Railway
+        port=int(os.environ.get("DB_PORT", 3306)),         # Puerto de Railway
+        user=os.environ.get("DB_USER", "root"),           # Usuario
+        password=os.environ.get("DB_PASSWORD", ""),       # Contraseña
+        database=os.environ.get("DB_NAME", "flask_db")    # Base de datos
     )
