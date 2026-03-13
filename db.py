d1c1d9e2c9314902aa814ceb372f4692f9c56fcf
LAB_B_REAL <labb@continental.edu.pe>
2026-03-13 15:55:36 -0500

    12

--- db.py
+++ db.py
@@ -2,11 +2,10 @@ import os
 import mysql.connector
 
 def get_connection():
-    # Conectamos usando las variables de entorno
     return mysql.connector.connect(
-        host=os.environ.get("DB_HOST", "localhost"),       # Host de Railway
-        port=int(os.environ.get("DB_PORT", 3306)),         # Puerto de Railway
-        user=os.environ.get("DB_USER", "root"),           # Usuario
-        password=os.environ.get("DB_PASSWORD", ""),       # Contraseña
-        database=os.environ.get("DB_NAME", "flask_db")    # Base de datos
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
